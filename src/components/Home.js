98a34135c20e592f336fe18d98d2c95f16872a52
kashishjivani <kashishjivani2002@gmail.com>
2024-10-13 19:14:40 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+// src/components/Home.js
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <div className="text-center text-white">
+        <h1 className="text-6xl font-bold mb-4">Coming Soon!</h1>
+        <p className="text-xl">We are working hard to get the site ready.</p>
+        <p className="text-xl">Stay tuned for updates!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
